65e2cc7d001a0c59746cc8858775fdacfdbb4a41
Rzmvski <victor.razumovski@gmail.com>
2022-12-19 00:57:35 +0300

    Add method create

--- src/main/java/app/Controller/UserController.java
+++ src/main/java/app/Controller/UserController.java
@@ -1,12 +1,36 @@
 package app.Controller;
 
+import app.Model.User;
+import app.Service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class UserController {
-    @GetMapping("/")
-    public String index() {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping()
+    public String index(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
         return "index";
+    }
+
+    //-----Create ------
+    @GetMapping("/add")
+    public String add(@ModelAttribute("user") User user) {
+        return "add";
+    }
+    @PostMapping()
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.add(user);
+        return "redirect:/";
     }
 }
